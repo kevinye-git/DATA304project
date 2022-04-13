aedf7e397e324f0d675e335599b5e432fa24f3b1
kevinye-git <99583033+kevinye-git@users.noreply.github.com>
2022-04-13 17:16:18 +1200

    New file to read data
    this is version 1 of the script to read the data. Will need updating to allow for more than 1 file to read at once

---
+++ read_data.py
@@ -0,0 +1,60 @@
+filename = 'KevinYeData-11-04-22'
+lines = []
+
+f = open(filename, 'r')
+while True:
+    line = f.readline()
+    if line == '':
+        break
+    lines.append(line)
+
+lines = lines[4:]
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+f.close()
+
+
+def find_event(data, event):
+    stop = False
+    time = None
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if lines[i][j] == event:
+                time = lines[i][0]
+                stop = True
+                break
+        if stop:
+            break
+    return time
+
+
+print(find_event(lines, 'a10000'))
+
+
+arrival_times = []
+cond = True
+i = 1
+while cond:
+    letter = 'a' + str(i)
+    if find_event(lines, letter) == None:
+        break
+    arrival_times.append(find_event(lines, letter))
+    i += 1
+interarrival_times = []
+for i in range(len(arrival_times)-1):
+    interarrival_time = float(arrival_times[i+1])-float(arrival_times[i])
+    interarrival_times.append(interarrival_time)
+
+service_times = []
+cond = True
+i = 1
+while cond:
+    letter = 'd' + str(i)
+    letter2 = 's' + str(i)
+    if find_event(lines, letter) == None or find_event(lines, letter2) == None:
+        break
+    service_times.append(float(find_event(lines, letter)) -
+                         float(find_event(lines, letter2)))
+    i += 1
+print(interarrival_times)
+print(service_times)
